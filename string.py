fa4c6b0c5895cd73873d2a758012cde1103a5fe8
Larryson Chijioke <larrysongift@gmail.com>
2026-06-22 10:39:36 +0100

    new changes

--- string.py
+++ string.py
@@ -2,14 +2,14 @@ names = 'Larryson Chijioke'
 print(names[0])  # L
 print(names[1])  # a
 print(names[2])  # r
-print(names.replace('o', 'a'))  # Larrysan Chijiake   
+print(names.replace('o', 'aa'))  # Larrysan Chijiake   
 
 
 # print(names.replace('o', 'a'))
 
-for i in range(len(names)):
-    print(i, names[i])
+# for i in range(len(names)):
+#     print(i, names[i])
 
 
-for name in names:
-    print(name.upper(), end=' ')
+# for name in names:
+#     print(name.upper(), end=' ')
