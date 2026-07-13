6111606556f0e8b2bb2f8d0d975069b00529c171
Larryson Chijioke <larrysongift@gmail.com>
2026-07-13 11:58:18 +0100

    func

--- functions.py
+++ functions.py
@@ -1,30 +1,71 @@
+# def displayName():
+#     print("Your name is John Doe")
+
+# displayName()
+
+
+
+# def displayNames(name):
+#     print('My name is : {name}')
+
+# displayNames('John Doe')
+
+
+# def sum(a,b):
+#     return a + b
+
+# add = sum(2,4)
+# print(add)
+
+
+# myName = str(input("Enter your name: ")).capitalize()
+# myTime = input("Enter your time, (Morning/Afternoon/Evening): ").upper()
+
+# def greet(name, time):
+#     print(f"Good {time}, {name}!")
+
+
+# greet(myName, myTime)
+
+
+
+
+# myName = input('Enter a name: ')
+# myTime = input('Enter a time: ')
+
+
+# def greet(name = 'No Name', time = 'No Time'):
+#     print(f"Good {time}, {name}! How are you doing today?")
+
+
+
+# greet()
+
+import math 
+
+def area(radius):
+    print(3.142 * pow(radius,2))
+rad = int(input('Enter a radius: '))
+
+area(rad)
+
+
+
+# name = 'Ogbonna '
+
+# def displayName():
+#     print(f'Your name inside the function is {name}')
+
+# print('Your name outside the function is', name)
+
+# displayName()
+
+
+name = 'Ogbonna'
 def displayName():
-    print("Your name is John Doe")
+    name = 'John Doe'
+    print(f'Your name inside the function is {name}')
+
+print('Your name outside the function is', name)
 
 displayName()
-
-
-
-def displayNames(name):
-    print('My name is : {name}')
-
-displayNames('John Doe')
-
-
-def sum(a,b):
-    return a + b
-
-add = sum(2,4)
-print(add)
-
-
-myName = str(input("Enter your name: ")).capitalize()
-myTime = input("Enter your time, (Morning/Afternoon/Evening): ").upper()
-
-def greet(name, time):
-    print(f"Good {time}, {name}!")
-
-
-greet(myName, myTime)
-
-  
