13d2b18431f99d1204366b84802de5dbc8316350
Larryson Chijioke <larrysongift@gmail.com>
2026-07-20 09:15:06 +0100

    added

--- Excercise.py
+++ Excercise.py
@@ -1,122 +1,122 @@
-#1
+1
 
-# name = input("What is your name: ")
+name = input("What is your name: ")
 
-# for i in range(100):
+for i in range(100):
 
-#     print(i, name)
+    print(i, name)
 
-    #2
+    2
 
-# name = 'Larryson Chijioke'
-# while  True:
-#     print(name, end=" ")
-
-
-#3
-
-# name = input("What is your name?: ")
-# for i in range(100):
-#     print(i+1, f"Your name is {name}" )
-
-#4
-
-# integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-# for integer in integers:
-#     print(f"{integer} ----",integer*integer)
+name = 'Larryson Chijioke'
+while  True:
+    print(name, end=" ")
 
 
-#5
+3
 
-# for i in range(8, 90, 3):
-#     print(i)
+name = input("What is your name?: ")
+for i in range(100):
+    print(i+1, f"Your name is {name}" )
+
+4
+
+integers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for integer in integers:
+    print(f"{integer} ----",integer*integer)
 
 
-#6
+5
 
-# for i in range(100, 1, -2):
-#     print(i)
+for i in range(8, 90, 3):
+    print(i)
 
-#7
 
-# for i in range(10):
-#     print("A", end="")
-# for i in range(7):
-#     print("B", end="")
-# for i in range(4):
-#     print("C", end="")
-#     print("D", end="")
-# print("E",end="")
-# for i in range(6):
-#     print("F", end="")
-# print("G", end="")
-# print()
+6
 
-#8
+for i in range(100, 1, -2):
+    print(i)
 
-# name = input("What is your name: ")
-# num = int(input("How many times do you want to print your name: "))
-# print(name * num,)
+7
+
+for i in range(10):
+    print("A", end="")
+for i in range(7):
+    print("B", end="")
+for i in range(4):
+    print("C", end="")
+    print("D", end="")
+print("E",end="")
+for i in range(6):
+    print("F", end="")
+print("G", end="")
+print()
+
+8
+
+name = input("What is your name: ")
+num = int(input("How many times do you want to print your name: "))
+print(name * num,)
     
-    #8
+    8
 
-# name = input("What is your name: ")
-# num = int(input("How many times do you want to print your name: "))
-# for i in range(num):
-#     print(i+1, name)
-
-
-#9
-# a = 1
-# b = 1
-# a,b = b,a
-# n = int(input("How many Fibonacci do you want printed?:  "))
-
-# for i in range(n):
-#     print(a, end=" ")
-#     a,b = b,a+b
-
-    #10
-# wide = int(input("How wide should the '*' print?: "))
-# high = int(input("How high should the '*' print?: "))
-
-# for i in range(high):
-#     print("*" * wide)
-
-#11
+name = input("What is your name: ")
+num = int(input("How many times do you want to print your name: "))
+for i in range(num):
+    print(i+1, name)
 
 
-# wide = int(input("How wide should the '*' print?: "))
-# height = int(input("How high should the '*' print?: "))
+9
+a = 1
+b = 1
+a,b = b,a
+n = int(input("How many Fibonacci do you want printed?:  "))
 
-# for row in range(height):
-#     if row == 0 or row == height - 1:
-#         print("*" * wide)
-#     else:
-#         print('*'+''*(wide-2)+ '*')
+for i in range(n):
+    print(a, end=" ")
+    a,b = b,a+b
 
-#12
-# height = int(input("How high should the '*' print?: "))
-# for i in range(height):
-#     print(i,'*' * i)
+    10
+wide = int(input("How wide should the '*' print?: "))
+high = int(input("How high should the '*' print?: "))
 
-#13
+for i in range(high):
+    print("*" * wide)
 
-# height = int(input("How high should the '*' print?: "))
-# for i in range(height):
-#         print(i,"*" * (height - i))
+11
 
-#14
-#We are trying to draw a diamond shape using asterics
 
-# height = int(input("How high should the '*' print?: "))
-# mid = height // 2
-# # First half (expanding)
-# for i in range(mid):
-#     print(" " * (mid - i - 1) + "*" * (2 * i + 1))
-# # Second half (contracting)
-# for i in range(mid, height):
-#     print(" " * (i - mid) + "*" * (2 * (height - i - 1) + 1))
+wide = int(input("How wide should the '*' print?: "))
+height = int(input("How high should the '*' print?: "))
+
+for row in range(height):
+    if row == 0 or row == height - 1:
+        print("*" * wide)
+    else:
+        print('*'+''*(wide-2)+ '*')
+
+12
+height = int(input("How high should the '*' print?: "))
+for i in range(height):
+    print(i,'*' * i)
+
+13
+
+height = int(input("How high should the '*' print?: "))
+for i in range(height):
+        print(i,"*" * (height - i))
+
+14
+We are trying to draw a diamond shape using asterics
+
+height = int(input("How high should the '*' print?: "))
+mid = height // 2
+# First half (expanding)
+for i in range(mid):
+    print(" " * (mid - i - 1) + "*" * (2 * i + 1))
+# Second half (contracting)
+for i in range(mid, height):
+    print(" " * (i - mid) + "*" * (2 * (height - i - 1) + 1))
 
 
 height = int(input("How high should the '*' print?: "))
--- function.py
+++ function.py
@@ -1,80 +1,80 @@
-# def calculate(quantity, price, discount_percent, tax_percent):
-#     subtotal = quantity * price
-#     discount = subtotal * (discount_percent / 100)
-#     amount_after_discount = subtotal - discount
-#     tax = amount_after_discount * (tax_percent / 100)
-#     total = amount_after_discount + tax
+def calculate(quantity, price, discount_percent, tax_percent):
+    subtotal = quantity * price
+    discount = subtotal * (discount_percent / 100)
+    amount_after_discount = subtotal - discount
+    tax = amount_after_discount * (tax_percent / 100)
+    total = amount_after_discount + tax
 
-#     print("\nReceipt")
-#     print("-------")
-#     print("Quantity:", quantity)
-#     print("Price per item:", price)
-#     print("Subtotal:", subtotal)
-#     print("Discount:", discount)
-#     print("Tax:", tax)
-#     print("Total amount to pay:", total)
+    print("\nReceipt")
+    print("-------")
+    print("Quantity:", quantity)
+    print("Price per item:", price)
+    print("Subtotal:", subtotal)
+    print("Discount:", discount)
+    print("Tax:", tax)
+    print("Total amount to pay:", total)
 
 
-# quantity = int(input("Enter quantity: "))
-# price = float(input("Enter price per item: "))
-# discount_percent = float(input("Enter discount percentage: "))
-# tax_percent = float(input("Enter tax percentage: "))
+quantity = int(input("Enter quantity: "))
+price = float(input("Enter price per item: "))
+discount_percent = float(input("Enter discount percentage: "))
+tax_percent = float(input("Enter tax percentage: "))
 
-# calculate(quantity, price, discount_percent, tax_percent)
+calculate(quantity, price, discount_percent, tax_percent)
 
-# job = "Doctor"
+job = "Doctor"
 
-# def occupation():
-#     global job
-#     job = 'Surgeon'
-#     print("My occupation is:", job)
+def occupation():
+    global job
+    job = 'Surgeon'
+    print("My occupation is:", job)
 
-# occupation()
+occupation()
 
-# print("I am still A :", job)
+print("I am still A :", job)
 
 def greeting(name="John Doe", time="day"):
     print("Hello, " , name , "Good,",time, "!!!!!")
 
 greeting()
 
-# greeting("Alice", "morning")
-# greeting("Bob", "Afternoon")
+greeting("Alice", "morning")
+greeting("Bob", "Afternoon")
 
 
-# import math
+import math
 
-# def area():
-#     radius = float(input("Enter the radius of the circle: "))
-#     area = math.pi  * pow(radius, 2)
-#     print("The area of the circle is:", round(area, -1))
+def area():
+    radius = float(input("Enter the radius of the circle: "))
+    area = math.pi  * pow(radius, 2)
+    print("The area of the circle is:", round(area, -1))
 
-# area()
+area()
 
-# b = 3
-# c = 5
-# a = 3
+b = 3
+c = 5
+a = 3
 
-# def add(a,b):
-#     return a + b
+def add(a,b):
+    return a + b
 
-# add(b, c)
-# print("The sum is:", add  )
+add(b, c)
+print("The sum is:", add  )
 
 
-# def area(radius):
-#     return 3.146 * radius * radius
+def area(radius):
+    return 3.146 * radius * radius
 
-# r = float(input("Enter the radius of the circle: "))
-# h = float(input("Enter the height of the circle: "))
+r = float(input("Enter the radius of the circle: "))
+h = float(input("Enter the height of the circle: "))
 
-# calc_area = area(r)
+calc_area = area(r)
 
-# def vol(a, h):
-#     print("The volume of the circle is:", a * h, )
+def vol(a, h):
+    print("The volume of the circle is:", a * h, )
 
-# vol(calc_area, h)
-# vol(area(r), h)
+vol(calc_area, h)
+vol(area(r), h)
 
 
 
